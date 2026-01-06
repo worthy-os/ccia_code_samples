5d64d8d5355f79379f7922564ecc7034ff5536fd
Grant Rostig <grant.github@emailmeinfo.com>
2026-01-05 22:39:58 -0600

    CppMSG meeting

--- listings/listing_3.2.cpp
+++ listings/listing_3.2.cpp
@@ -1,7 +1,6 @@
 #include <mutex>
 
-class some_data
-{
+class some_data{
     int a;
     std::string b;
 public:
@@ -9,36 +8,34 @@ public:
     {}
 };
 
-class data_wrapper
-{
+class data_wrapper {
 private:
     some_data data;
     std::mutex m;
 public:
-    template<typename Function>
-    void process_data(Function func)
-    {
+    template<typename Function> // 
+	requires std::is_invocable_v<Function, some_data&>
+    void process_data(Function func)    {
         std::lock_guard<std::mutex> l(m);
         func(data);
     }
 };
 
 some_data* unprotected;
-
-void malicious_function(some_data& protected_data)
-{
+void malicious_function(some_data& protected_data){
     unprotected=&protected_data;
 }
 
-data_wrapper x;
+void nice_function(some_data& protected_data) {
+    protected_data.do_something();
+}
 
-void foo()
-{
+data_wrapper x;
+void foo(){
     x.process_data(malicious_function);
     unprotected->do_something();
 }
 
-int main()
-{
+int main(){
     foo();
 }
