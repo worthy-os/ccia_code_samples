8f5ccb1c14949ab302a3ea21f09232b0e5a86707
Grant Rostig <grant.github@emailmeinfo.com>
2026-01-05 21:48:00 -0600

    grants changes

--- listings/listing_c.all.cpp
+++ listings/listing_c.all.cpp
@@ -473,16 +473,7 @@ int main() {
     while(!quit_pressed) {
         char c=getchar();
         switch(c) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
             atmqueue.send(digit_pressed(c));
             break;
         case 'b':
